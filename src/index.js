8ef74d136d1099d479b27e5b3df63cddb8ae099a
ThiNguyen <dragon_thi46@yahoo.com>
2022-01-07 10:06:29 +0700

    change

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const svgIcon = '<svg class="hero-pagination-item" width="65px" height="65px" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg"><circle stroke-width="9" fill="none" stroke-linecap="round" cx="33" cy="33" r="28"></circle> <circle class="time" stroke-width="9" fill="none" stroke-linecap="round" cx="33" cy="33" r="28" ></circle></svg>'
+const swiperHero = new Swiper('.swiperHero', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+
+    },
+    speed: 300,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    parallax: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function(index, className) {
+            return `<span  class='${className}'> ${svgIcon} </span>`;
+        }
+    },
+});
+
+
+if ($(window).width() > 1200) {
+    var rellax = new Rellax(".parallax-image");
+}
+// $('.parallax-image').Rellax()
